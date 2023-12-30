533516c9dad32c3d1cd35a2108e3707e72b83323
Shantanu Kumar <155242837+shantanu7797@users.noreply.github.com>
2023-12-30 22:37:18 +0530

    Create Thala For A Reason.cpp

---
+++ Thala For A Reason.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    if(n==7){
+        cout<<"THALA"<<endl;
+    }
+    else{
+        cout<<"SADGE"<<endl;
+    }
+	// your code goes here
+	return 0;
+}
